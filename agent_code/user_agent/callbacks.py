b945587a61c8914f894d606a554546442dbf091e
Alexander Kunkel <kunkel.alexander@protonmail.com>
2025-06-13 19:53:03 +0200

    The Cratehero has learned how to use bombs without killing himself

--- agent_code/user_agent/callbacks.py
+++ agent_code/user_agent/callbacks.py
@@ -1,7 +1,10 @@
+from .helpers import ascii_pictogram, state_to_features, describe_state
 def setup(self):
     pass
 
 
 def act(self, game_state: dict):
     self.logger.info('Pick action according to pressed key')
+    features = state_to_features(game_state)
+    print(describe_state(features))
     return game_state['user_input']
